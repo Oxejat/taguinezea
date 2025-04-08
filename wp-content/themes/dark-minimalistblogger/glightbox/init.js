287e5c1b4ede67e4bad8e0a5e0652df19a3a854e
Oxejat <142019205+Oxejat@users.noreply.github.com>
2025-04-08 16:59:50 +0100

    Staatic deployment d076d834d4a1

--- wp-content/themes/dark-minimalistblogger/glightbox/init.js
+++ wp-content/themes/dark-minimalistblogger/glightbox/init.js
@@ -4,3 +4,16 @@ loop: true,
 autoplayVideos: true,
 zoomable: true
 });
+standardLightbox.on('slide_after_load', (data) => {
+
+    const { slideIndex, slideNode, slideConfig, player, trigger } = data;
+   
+    var btnClicked = document.getElementById(trigger.id);
+    var posterimage = btnClicked.getAttribute('data-html5videoposter');
+    var videoid = btnClicked.getAttribute('data-videoid');
+
+    // use the index
+    var getVideoTag = document.getElementById(videoid);
+    getVideoTag.setAttribute('poster', posterimage);
+
+});
